01068b5789e1745ca317551d8da626cc0bce8702
yoyosource <yoyosource@nidido.de>
2023-08-02 22:07:31 +0200

    Update updateMD.sh

--- updateMD.sh
+++ updateMD.sh
@@ -8,7 +8,7 @@ echo "" >> README.md
 
 IFS=$'\n'
 
-for plugin in $(ls */* | grep -e ":"); do
+for plugin in $(ls */* | grep -e ":" | sort); do
     pluginName="${plugin#??}"
     echo "- ${pluginName%?}" >> README.md
 done
